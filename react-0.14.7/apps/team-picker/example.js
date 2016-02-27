167b786e56910c86ba2ee742ba6f6f8d9d137049
Nat <nat@meraki.com>
2016-02-26 19:23:27 -0800

    Updated styling, start of better matchups

--- react-0.14.7/apps/team-picker/example.js
+++ react-0.14.7/apps/team-picker/example.js
@@ -13,11 +13,10 @@ var Application = React.createClass({
       "Nat",
       "Raphael",
       "Raymond",
-      "Sam",
     ];
     var players = _.map(names, function(n) {
       return {
-        name: n,
+        name: n.toUpperCase(),
       }
     });
     return {
@@ -33,13 +32,25 @@ var Application = React.createClass({
   },
   addPlayer(e) {
     e.preventDefault();
-    var name = this.refs.newName.value;
+    var name = this.refs.newName.value.toUpperCase();
     var present = _.findWhere(this.state.players, { name: name });
     if (!present && name != "") {
       var new_players = _.union(this.state.players, [{ name: name }]);
       this.setState({ players: new_players });
     }
     return false;
+  },
+  generateMatchup() {
+    var players = _.shuffle(this.state.players);
+    players = _.sortBy(players, function(p) { return p.streak; });
+    players = _.sortBy(players, function(p) { return p.plays; });
+    players = players.slice(0,9);
+    var team1 = _.sample(players, 4);
+    var team2 = _.sample(_.difference(players, team1), 4);
+    var sitting = _.difference(this.state.players, team1, team2);
+    var games = this.state.games;
+    games.push([team1, team2, sitting]);
+    this.setState({ games: games });
   },
   render() {
     var _this = this;
@@ -50,32 +61,63 @@ var Application = React.createClass({
                      deleteCallback={ _this.removePlayer }/>
       );
     });
+    var games = _.map(this.state.games, function(g, index) {
+      return <Game num={ index } teams={ g } />
+    });
     var team = _.sample(this.state.players, 4);
     return(
       <div>
-        { panels }
-        <form onSubmit={ this.addPlayer }>
-          <span>Add player </span>
-          <input ref="newName" type='text'></input>
-        </form>
-        <TeamList name="Team 1" players={ team }/>
+        <div classsName="Roster">
+          <h3>ROSTER</h3>
+          { panels }
+          <form onSubmit={ this.addPlayer }>
+            <span>ADD PLAYER </span>
+            <input ref="newName" type='text'></input>
+          </form>
+        </div>
+        { games }
+        <span className="matchup" onClick={ this.generateMatchup }>NEXT MATCHUP</span>
       </div>
     );
   }
 });
 
 var PlayerPanel = React.createClass({
-  render() {
-    var statusClass = classNames({
-      'PlayerStatus': true,
-      'active': this.props.status === "active",
-      'first': this.props.status === "first",
-      'absent': this.props.status === "absent",
-    });
+  renderStats() {
+    if (!this.props.deleteCallback) return null;
     return (
-      <div className="PlayerPanel">
-        <span>{ this.props.name }</span>
-        { this.props.deleteCallback && <button onClick={ this.props.deleteCallback.bind(null, this.props.name) }>Remove</button> }
+      <span className="StatsWrapper">
+        <span className="played">{ 4 }</span>
+        <span className="sat">{ 1 }</span>
+        <span className="streak">{ 2 }</span>
+        <span className="spacer">0</span>
+        <span className="remove" onClick={ this.props.deleteCallback.bind(null, this.props.name) }>X</span>
+      </span>
+    );
+  },
+  render() {
+    var stats = this.props.deleteCallback;
+    return (
+      <div>
+        <div className="PlayerPanel">
+          <span className="name">{ this.props.name }</span>
+          { this.renderStats() }
+        </div>
+      </div>
+    );
+  }
+});
+
+var Game = React.createClass({
+  render() {
+    console.log("rendering game");
+    return (
+      <div>
+        <hr/>
+        <h3>GAME { this.props.num }</h3>
+        <TeamList name="HOME" players={ this.props.teams[0] }/>
+        <TeamList name="AWAY" players={ this.props.teams[1] }/>
+        <TeamList name="OUT" players={ this.props.teams[2] }/>
       </div>
     );
   }
@@ -84,10 +126,7 @@ var PlayerPanel = React.createClass({
 var TeamList = React.createClass({
   render() {
     var players = _.map(this.props.players, function(p, index) {
-      return (
-        <PlayerPanel name={ p.name }
-                     key={ "player_" + index }/>
-      );
+      return <PlayerPanel name={ p.name }/>
     });
     return (
       <div className="TeamList">
